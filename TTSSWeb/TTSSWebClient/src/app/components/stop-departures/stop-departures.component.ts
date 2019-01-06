b3f090aa86fa53af3dd6c141e21301374b467a59
Dominik Szymański <domints@gmail.com>
2019-01-06 17:31:30 +0100

    add refresher to stop passages list

--- TTSSWeb/TTSSWebClient/src/app/components/stop-departures/stop-departures.component.ts
+++ TTSSWeb/TTSSWebClient/src/app/components/stop-departures/stop-departures.component.ts
@@ -6,6 +6,8 @@ import { SavePassageDialogComponent } from '../save-passage-dialog/save-passage-
 import { IRoutableComponent } from 'src/app/interfaces/IRoutableComponent';
 import { DepartureDataService } from 'src/app/services/store-services/departure-data.service';
 import { Router } from '@angular/router';
+import { interval } from 'rxjs/internal/observable/interval';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'stop-departures',
@@ -17,9 +19,15 @@ export class StopDeparturesComponent implements OnInit, IRoutableComponent {
   onRouteIn() {
     this.stopValueEvents = true;
     this.departureDataService.restore(this);
+    if(this.currentStop)
+    {
+      this.refreshPassages();
+      this.startRefresher();
+    }
     this.stopValueEvents = false;
   }
   onRouteOut() {
+    this.stopRefresher();
     this.departureDataService.store(this);
   }
   showBackArrow: boolean = false;
@@ -30,6 +38,8 @@ export class StopDeparturesComponent implements OnInit, IRoutableComponent {
   passages: PassageListItem[] = [];
   currentPassages: PassageListItem[] = [];
   oldPassages: PassageListItem[] = [];
+
+  refresherSubscription: Subscription;
 
   constructor(private stopsService: StopsService,
     private dialog: MatDialog,
@@ -43,15 +53,32 @@ export class StopDeparturesComponent implements OnInit, IRoutableComponent {
           this.stopsService.getAutocomplete(v).subscribe(r => this.autocompleteOptions = r);
         else
         {
+          this.stopRefresher();
           this.currentStop = v;
-          this.stopsService.getPassages(v.id).subscribe(r => {
-            this.passages = r;
-            this.currentPassages = r.filter(p => !p.isOld);
-            this.oldPassages = r.filter(p => p.isOld);
-          });
+          this.refreshPassages();
+          this.startRefresher();
           this.toolbarTitle = "Odjazdy - " + v.name;
         }
       }
+    });
+  }
+
+  startRefresher() {
+    if(!this.refresherSubscription || this.refresherSubscription.closed)
+      this.refresherSubscription = interval(20000).subscribe(() => this.refreshPassages());
+  }
+
+  stopRefresher() {
+    if(this.refresherSubscription && !this.refresherSubscription.closed)
+      this.refresherSubscription.unsubscribe();
+  }
+
+  refreshPassages()
+  {
+    this.stopsService.getPassages(this.currentStop.id).subscribe(r => {
+      this.passages = r;
+      this.currentPassages = r.filter(p => !p.isOld);
+      this.oldPassages = r.filter(p => p.isOld);
     });
   }
 
