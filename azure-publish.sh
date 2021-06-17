9233825a318c24d09c003355f11ae4130c866a22
Dominik Szymański <domints@gmail.com>
2021-06-17 15:52:34 +0200

    Remove ssh key and copy pushlish script to artifacts

--- azure-publish.sh
+++ azure-publish.sh
@@ -1,5 +1,5 @@
-cd ~/deploy_temp
-sudo /usr/sbin/service $1 stop
-rm -rf /var/aspnetcore/$1/*
-tar -xzf artifact.tar.gz -C /var/aspnetcore/$1
+cd ~/deploy_temp && \
+sudo /usr/sbin/service $1 stop && \
+rm -rf /var/aspnetcore/$1/* && \
+tar -xzf artifact.tar.gz -C /var/aspnetcore/$1 && \
 sudo /usr/sbin/service $1 start
