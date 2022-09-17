da8c3b1a4ad4c6a86aabe8d97f6d232b4a0ffade
Dominik Szymański <domints@gmail.com>
2022-09-17 18:53:46 +0200

    Revert "Update publish script to include messages"
    This reverts commit 414babd63c93c9bf317ac80bbb043b049b6a249c.

---
+++ DiffCalc/ServiceAlerts_T.pb
@@ -0,0 +1,8 @@
+
+
+2.0÷ü®†Ù
+!ActionId(2 ActIdx:39 ActSubIdx:0)*³R
+
+OgÃ³lny tekst specjalnyZ“
+
+Remont torowiska na ulicy Lubicz i Rakowickiej * Linie 2, 4, 10, 14, 20 i 52 kursujÄ… po trasach objazdowych * SzczegÃ³Å‚y na www.kmkrakow.pl
--- azure-publish.sh
+++ azure-publish.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 cd ~/deploy_temp && \
-echo "Stopping ttss service..." && \
 sudo /usr/sbin/service ttss stop && \
-echo "TTSS service stopped. Removing old app" && \
 rm -rf /var/dotnet/ttss/* && \
-echo "Old version removed. Unzipping artifacts" && \
 tar -xzf artifact.tar.gz -C /var/dotnet/ttss && \
-echo "Artifacts unzipped. Starting TTSS service..." && \
-sudo /usr/sbin/service ttss start && \
-echo "Deployment script finished!"
+sudo /usr/sbin/service ttss start
