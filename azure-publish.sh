4e8e7ed1382663f251756d7e8529246e414c2af0
Dominik Szymański <domints@gmail.com>
2022-09-17 20:06:50 +0200

    Fix deployment script to use executable scripts

--- azure-publish.sh
+++ azure-publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd ~/deploy_temp && \
 echo "Stopping ttss service..." && \
-sudo /usr/bin/systemctl start ttss && \
+sudo /usr/bin/systemctl_start ttss && \
 echo "TTSS service stopped. Removing old app" && \
 rm -rf /var/dotnet/ttss/* && \
 echo "Old version removed. Unzipping artifacts" && \
 tar -xzf artifact.tar.gz -C /var/dotnet/ttss && \
 echo "Artifacts unzipped. Starting TTSS service..." && \
-sudo /usr/bin/systemctl start ttss && \
+sudo /usr/bin/systemctl_start ttss && \
 echo "Deployment script finished!"
