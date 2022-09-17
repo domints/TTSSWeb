414babd63c93c9bf317ac80bbb043b049b6a249c
Dominik Szymański <domints@gmail.com>
2022-09-17 18:32:26 +0200

    Update publish script to include messages

--- azure-publish.sh
+++ azure-publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd ~/deploy_temp && \
+echo "Stopping ttss service..." && \
 sudo /usr/sbin/service ttss stop && \
+echo "TTSS service stopped. Removing old app" && \
 rm -rf /var/dotnet/ttss/* && \
+echo "Old version removed. Unzipping artifacts" && \
 tar -xzf artifact.tar.gz -C /var/dotnet/ttss && \
-sudo /usr/sbin/service ttss start
+echo "Artifacts unzipped. Starting TTSS service..." && \
+sudo /usr/sbin/service ttss start && \
+echo "Deployment script finished!"
